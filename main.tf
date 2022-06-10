0824b262398479a62352ba0cc5bcf74683de1e43
Alex Emanuel <alexprojetos27@gmail.com>
2022-06-10 15:42:38 -0300

    Update terraform to 0.13.1

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_kinesis_stream" "kinesis_stream1" {
-    name = "${var.name}"
-    shard_count = "${var.shard_count}"
-    retention_period = "${var.retention_period}"
+    name = var.name
+    shard_count = var.shard_count
+    retention_period = var.retention_period
 }
--- provider.tf
+++ provider.tf
@@ -1,4 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
 provider "aws" {
-  region     = "us-east-1"
-  version = "v2.70.0"
+  region = "us-east-1"
 }
