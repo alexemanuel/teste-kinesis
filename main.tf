3a276686f243db8d8715a3f8c8775d3409e825a1
Alex Emanuel <alexprojetos27@gmail.com>
2022-06-10 15:14:05 -0300

    remove "stream_mode_details" from aws_kinesis_stream

--- main.tf
+++ main.tf
@@ -2,7 +2,4 @@ resource "aws_kinesis_stream" "kinesis_stream1" {
     name = "${var.name}"
     shard_count = "${var.shard_count}"
     retention_period = "${var.retention_period}"
-    stream_mode_details {
-        stream_mode = "${var.stream_mode}"
-    }
 }
