95fa5fdbd1a48732a5faf46efbdd062c55e5d693
Alex Emanuel <alexprojetos27@gmail.com>
2022-06-10 15:10:15 -0300

    set default value to all variables

--- variables.tf
+++ variables.tf
@@ -5,12 +5,15 @@ variable "name"{
 
 variable "shard_count"{
     description = "shard_count"
+    default = "1"
 }
 
 variable "retention_period"{
     description = "retention_period" 
+    default = "24"
 }
 
 variable "stream_mode"{
     description = "stream_mode"
+    default = "PROVISIONED"
 }
