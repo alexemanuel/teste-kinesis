8f0f2e2d4893a7d93c5cb3583dd178339a12e9e0
Alex Emanuel <alexprojetos27@gmail.com>
2022-06-10 14:36:55 -0300

    Remove types from variables

--- variables.tf
+++ variables.tf
@@ -1,20 +1,16 @@
 variable "name"{
-    type = "string"
     description = "name"
     default = "stream1"
 }
 
 variable "shard_count"{
-    type = "number"
     description = "shard_count"
 }
 
 variable "retention_period"{
-    type = "number"
     description = "retention_period" 
 }
 
 variable "stream_mode"{
-    type = "string"
     description = "stream_mode"
 }
