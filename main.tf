e8d459630e4b3b2815883c2adc72cb0575a9bd78
Alex Emanuel <alexprojetos27@gmail.com>
2022-06-10 14:27:14 -0300

    Rollback old terraform verison

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 resource "aws_kinesis_stream" "kinesis_stream1" {
-    name = var.name
-    shard_count = var.shard_count
-    retention_period = var.retention_period 
+    name = "${var.name}"
+    shard_count = "${var.shard_count}"
+    retention_period = "${var.retention_period}"
     stream_mode_details {
-        stream_mode = var.stream_mode
+        stream_mode = "${var.stream_mode}"
     }
 }
