e1878d2aa791cde7932515e904ca4dd8b2d4d6f0
Alex Emanuel <alexprojetos27@gmail.com>
2022-06-10 13:42:31 -0300

    Update terraform version to 1.2.2

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_kinesis_stream" "kinesis_stream1" {
-    name = "${var.name}"
+    name = var.name
     shard_count = var.shard_count
     retention_period = var.retention_period 
     stream_mode_details {
