79d7a9378dd2216700975d12e209d28669207d80
Alex Emanuel <alexprojetos27@gmail.com>
2022-06-13 17:08:34 -0300

    update kinesis

--- main.tf
+++ main.tf
@@ -2,4 +2,16 @@ resource "aws_kinesis_stream" "kinesis_stream2" {
     name = var.name
     shard_count = var.shard_count
     retention_period = var.retention_period
+
+    shard_level_metrics = [
+        "IncomeBytes",
+        "OutgoingBytes"
+    ]
+
+    stream_mode_details {
+        strem_mode = "PROVISIONED"
+    }
+    tags = {
+        nome =  "Alex"
+    }
 }
