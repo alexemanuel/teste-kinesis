c2e2f2567950bf8097d7440a9c807e87a1821481
Alex Emanuel <alexprojetos27@gmail.com>
2022-06-13 18:00:45 -0300

    opkpokopopk

--- main.tf
+++ main.tf
@@ -2,6 +2,8 @@ resource "aws_kinesis_stream" "kinesis_stream2" {
     name = var.name
     shard_count = var.shard_count
     retention_period = var.retention_period
+    encryption_type = "KMS"
+    kms_key_id = "alias/aws/kinesis"
 
     shard_level_metrics = [
         "IncomingBytes",
